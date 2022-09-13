7fd70728ff3c810653784ce570370fb61d1d0b9e
Nathalia Guerra <nathaliaguerra@Arthurs-MacBook-Pro.local>
2022-09-13 10:29:30 +0200

    Solution Problem 1

---
+++ Exercises/Problem1.py
@@ -0,0 +1,9 @@
+#Problem 1
+#This module executes the sum of two given numbers: A and B
+#To execute, use function 'sum_method()'
+
+def sum_method(A,B):
+    C = A + B
+    result = 'The result of ' + str(A) + ' + ' + str(B) + ' is ' + str(C) + '.'
+    print(result)
+
